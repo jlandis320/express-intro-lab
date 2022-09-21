c62ef4ef89560ebfd14ad435f26e008f46bd7841
jlandis320 <jreynoldslandis@gmail.com>
2022-09-20 21:13:37 -0400

    creates repo

---
+++ server.js
@@ -0,0 +1,23 @@
+// import modules
+import express from 'express'
+
+// create Express app
+const app = express()
+
+// configure the app
+
+// mount middleware
+
+// mount routes
+// Mount routes
+
+app.get('/', function(req, res) {
+  res.send('<h1>hello, friend</h1>')
+})
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function(){
+  console.log('Listening on port 3000');
+})
+
